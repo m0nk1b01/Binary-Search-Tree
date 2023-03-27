1a15d038aab1cd70bb75650df6f4bf0ea406818e
sbuckwalter <96084214+m0nk1b01@users.noreply.github.com>
2023-03-27 15:22:25 -0400

    Update binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -1,1 +1,32 @@
+#ifndef BINARYTREE_H_INCLUDED
+#define BINARYTREE_H_INCLUDED
 
+#ifndef ERROR
+#define ERROR -1
+#endif // ERROR
+
+typedef struct node
+    {
+        int data;
+        struct node* left;
+        struct node* right;
+    }node_type;
+
+typedef struct binary_search_tree
+    {
+        struct node* root;
+    }bst;
+
+char* search_tree(char*);
+void traverse_tree();
+void sort_tree();
+void add_node();
+void remove_node();
+node_type find_base();
+int depth_check();
+int* create_array();
+void print_array(int arr[]);
+
+
+
+#endif // BINARYTREE_H_INCLUDED
