07cbcdde17bf1515b311207110de3c6a9d05e9d1
sbuckwalter <96084214+m0nk1b01@users.noreply.github.com>
2023-03-26 22:24:56 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "BinaryTree.h"
-
-#define ERROR -1;
+#include "binary_tree.h"
 
 
-//Declaration of nodes containing the 15 most common words
-/*
-node_type level4_0 = {"a", 0L, 0L};
-node_type level4_1 = {"are", 0L, 0L};
-node_type level4_2 = {"he", 0L, 0L};
-node_type level4_3 = {"is", 0L, 0L};
-node_type level4_4 = {"of", 0L, 0L};
-node_type level4_5 = {"that", 0L, 0L};
-node_type level4_6 = {"to", 0L, 0L};
-node_type level4_7 = {"you", 0L, 0L};
-
-
-
-node_type level3_0 = {"and", &level4_0, &level4_1};
-node_type level3_1 = {"in", &level4_2, &level4_3};
-node_type level3_2 = {"on", &level4_4, &level4_5};
-node_type level3_3 = {"was", &level4_6, &level4_7};
-
-
-
-node_type level2_0 = {"for", &level3_0, &level3_1};
-node_type level2_1 = {"the", &level3_2, &level3_3};
-
-
-
-node_type root = {"it", &level2_0, &level2_1};
-*/
 
 int* create_array()
 {
-    int* bst_array[20];
-    for(int i = 0; i < sizeof(bst_array); i++)
+
+    int* bst_array[31];
+    srand(time(NULL));
+    for(int i = 0; i < sizeof(bst_array)/8; i++)
     {
-        bst_array[i] = rand() % sizeof(bst_array)*4;
+        bst_array[i] = rand() % sizeof(bst_array);
+        printf("%d, ", bst_array[i]);
     }
+
     return bst_array;
 }
 
+void print_array(int arr[6])
+{
+    int arr_len = sizeof(arr)/8;
+    printf("%d", arr_len);
+    printf("[");
+    for(int i = 0; i < arr_len - 1; i++)
+    {
+        printf("test");
+        printf("%d,", arr[i]);
+    }
+    printf("]\n");
+}
 
+void sort_array(int* bst_array)
+{
+    int temp = 0;
+    int value = 0;
+    int arr_len = sizeof(bst_array)/8;
+    int min_index = 0;
+    print_array(bst_array);
+    for(int i = 0; i < arr_len - 1; i++) //iterate through array
+    {
+        min_index = i;
+        for(int x = 0; x < arr_len - 1; x++)
+        {
+            if(bst_array[x] < bst_array[min_index])
+            {
+                printf("%d is less than %d", bst_array[x], bst_array[min_index]);
+                min_index = x;
+            }
+        }
+        temp = bst_array[i];
+        bst_array[i] = bst_array[min_index];
+        bst_array[min_index] = temp;
+    }
+
+    print_array(bst_array);
+
+
+}
+
+
+void build_tree()
+{
+    //re-oder starting list based on how far it is from the median value then build tree
+
+}
+/*
 char* search_tree(char* target)
 {
-    int depth = depth_check();//Height of the tree
-    node_type* on = bst->root; //This is for reference as to what node is being looked at
+    int depth = depthCheck();//Height of the tree
+    node_type* on = &root; //This is for reference as to what node is being looked at
 
     for(int i = 0; i < depth; i++)//loop through each layer of the tree
     {
         if(strcmp(target, on->word) == 0) //compares target(the word being searched for) and the the current word
         {
-            return on->word;
+            return on->definition;
         }
 
         if(strcmp(target, on->word) < 0) //if alphabetically before it will go to the left of the node
@@ -65,47 +87,28 @@ char* search_tree(char* target)
         }
 
     }
-    return "error";
+
 }
+*/
 
-node_type* onn = &root;
-
-
-void traverse_tree()
+/*
+void set_parent()
 {
-
-    if(onn->left != 0L)
+    node_type* on = findBase(); //set the on node to the base
+    while(1)
     {
-        printf("%s\n",onn->word);
-        onn = onn->left;
-        traverse_tree();
-
-    }else if(onn->left == 0L)
-    {
-        printf("found the end");
+        //printf("%s", on.)
+        if(on->next == 0L)
+        {
+            break;
+        }
     }
 }
-
-void sort_tree()
-{
-    //
-}
-
-void add_node()
-{
-    //node_type temp;
-    //temp = malloc(sizeof(node_type));
-    //findNewNode()
-}
-
-void remove_node()
-{
-    //remove a node then call sortTree()
-}
-
+*/
+/*
 node_type find_base() //trying to return the base of the linked list(the left most node)
 {
-    node_type* on = &root;
+    node_type* on = tree->root;
     while(1)
     {
         if(on->left == 0L)
@@ -115,8 +118,17 @@ node_type find_base() //trying to return the base of the linked list(the left mo
         on = on->left;
     }
 }
+*/
+void add_node()
+{
+    //add a node then call sortTree()
+}
 
-
+void remove_node()
+{
+    //remove a node then call sortTree()
+}
+/*
 int depth_check()
 {
     node_type* on = &root;
@@ -130,22 +142,21 @@ int depth_check()
 
     return depth;
 }
+*/
+
 int main()
 {
-    char word[32];//input the word being searched for
-    printf("Select one of these words: \n and \n are \n for \n he \n in \n is \n it \n of \n on \n that \n the \n to \n was \n you\n--------------------------------------------\n");
-    scanf("%s", word);
-    if(search_tree(word) != "error")
-    {
-        printf("I found the word: %s\n", search_tree(word));
-    }else
-    {
-        return ERROR;
-    }
-
+    bst tree;
+    //int* bst_array[31];
+    //bst_array = create_array();
+    //printf("%d", bst_array[0]);
+    int arr = {9, 3, 6, 1, 7};
+    print_array(arr);
+    //sort_array(create_array());
     return 0;
 
 }
+
 
 
 
